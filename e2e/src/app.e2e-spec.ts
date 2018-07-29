17040c56c3d516d5831ddc3c51e68a4ea5f71064
aleh.autushka <aleh.autushka@gmail.com>
2018-07-29 19:23:09 -0400

    test.html updated

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,39 @@
-import { AppPage } from './app.po';
+import {AppPage} from './app.po';
+import {browser, by, element} from 'protractor';
+import {RegressionSlopeValidator} from '@angular/benchpress';
+
+const benchpress = require('@angular/benchpress');
+const runner = new benchpress.Runner([
+  // use protractor as Webdriver client
+  benchpress.SeleniumWebDriverAdapter.PROTRACTOR_PROVIDERS,
+  // use RegressionSlopeValidator to validate samples
+  {provide: benchpress.Validator, useExisting: benchpress.RegressionSlopeValidator},
+  // use 10 samples to calculate slope regression
+  {provide: benchpress.RegressionSlopeValidator.SAMPLE_SIZE, useValue: 1},
+  // use the script metric to calculate slope regression
+  {provide: benchpress.RegressionSlopeValidator.METRIC, useValue: 'scriptTime'},
+  {provide: benchpress.Options.FORCE_GC, useValue: true}
+]);
+
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+  // let page: AppPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    // page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to streaming-poc!');
+  it('should display welcome message', (done) => {
+    runner.sample({
+      execute: () => {
+        browser.get(`/`);
+      },
+      userMetrics: {
+        timeToBootstrap: 'The time in milliseconds to bootstrap'
+      },
+      providers: [
+        {provide: RegressionSlopeValidator.METRIC, useValue: 'timeToBootstrap'}
+      ]
+    }).then(done, done.fail);
   });
 });
